941fec5c4f44a1d47c96015253e950338c674658
Jose <101306680+JSantosCEI@users.noreply.github.com>
2024-07-17 15:24:40 -0400

    added functions for adding user and getting user data rename function and changed travelers to optional JSON type

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,33 @@
+"use server"
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from "@vercel/postgres";
 import * as schema from './schema';
 import { eq } from 'drizzle-orm';
+import { Event } from '~/app/utilities/types';
 
 // Use this object to send drizzle queries to your DB
-export const db = drizzle(sql, { schema });
+const db = drizzle(sql, { schema });
 
-export const getAllUserEvent = async (id: string) => {
+export const getAllUserData = async (id: string) => {
+    return await db.select()
+        .from(schema.users)
+        .where(eq(schema.events.userid, id));
+}
+
+export const addNewUser = async (id: string, city?: string) => {
+    await db.insert(schema.users).values({
+        userId: id,
+        city,
+    });
+}
+
+export const getAllUserEvents = async (id: string) => {
     return await db.select()
         .from(schema.events)
         .where(eq(schema.events.userid, id));
 }
 
-export const addNewEvent = async (id: string, destination: string, startDate: string, endDate: string, travelers: string) => {
+export const addNewEvent = async (id: string, destination: string, startDate: string, endDate: string, travelers?: JSON) => {
     await db.insert(schema.events).values({
         userid: id,
         destination,
