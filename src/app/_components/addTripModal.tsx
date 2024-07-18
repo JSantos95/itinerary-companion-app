7f47b8dd1f89d12d602611535f1d5263f11d6787
Jose <101306680+JSantosCEI@users.noreply.github.com>
2024-07-18 12:48:13 -0400

    fixed the centering of the modal added backdrop blur

--- src/app/_components/addTripModal.tsx
+++ src/app/_components/addTripModal.tsx
@@ -44,8 +44,8 @@ export default function AddTripModal({ id, disableATPopup }: any) {
 
     return (
         <div id="authentication-modal" tabIndex={-1} aria-hidden="true"
-            className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div className="relative p-4 w-full max-w-md max-h-full">
+            className="overflow-y-auto overflow-x-hidden backdrop-blur-sm fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div className="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 w-full max-w-md max-h-full">
 
                 <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
 
