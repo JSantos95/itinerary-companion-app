53dffc2d4769a6876b9c3a91f9d8e62c08bcd1fe
Jose <santosjosel1995@gmail.com>
2024-07-19 21:28:16 -0400

    Sending props to the detail page with query deconstructed props for cleaner code

--- src/app/_components/Trip.tsx
+++ src/app/_components/Trip.tsx
@@ -2,16 +2,17 @@ import { FC } from "react";
 import { Event } from "../utilities/types";
 import Link from "next/link";
 
-export const Trip: FC<Event> = (props) => {
+export const Trip: FC<Event> = ({ userid, destination, startDate, endDate, id }) => {
 
     return (
         <div className="mb-3">
-            <Link href={"/event/" + props.id} className="block max-w-sm p-6 bg-white border border-blue-200 rounded-lg shadow hover:bg-blue-100 dark:bg-blue-600 dark:border-blue-600 dark:hover:bg-blue-800">
+            <Link href={{ pathname: "/event/" + id, query: { userid, destination, startDate, endDate, id } }}
+                className="block max-w-sm p-6 bg-white border border-blue-200 rounded-lg shadow hover:bg-blue-100 dark:bg-blue-600 dark:border-blue-600 dark:hover:bg-blue-800">
                 <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {props.destination}
+                    {destination}
                 </p>
                 <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {props.startDate.slice(5)} - {props.endDate.slice(5)}
+                    {startDate.slice(5)} - {endDate.slice(5)}
                 </p>
             </Link>
         </div>
