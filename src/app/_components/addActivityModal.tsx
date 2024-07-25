548a89cf30c04281e09a77dfdcd6f65652a27010
Jose <santosjosel1995@gmail.com>
2024-07-24 23:04:01 -0400

    Added the UI for adding a new activity

---
+++ src/app/_components/addActivityModal.tsx
@@ -0,0 +1,83 @@
+import { ChangeEvent, useState } from "react";
+import { addNewActivies, } from "~/server/db";
+
+export default function AddActivityModal({ id, disableATPopup }: any) {
+    const [name, setName] = useState('');
+    const [location, setLocation] = useState('');
+    const [type, setType] = useState('');
+    const [notes, setNotes] = useState('');
+    const [errorLog, setErrorLog] = useState('');
+
+    const sumbitNewTrip = async () => {
+        if (name == '' || location == '' || type == '') {
+            setErrorLog("Required field cannot be empty");
+        } else {
+            //make query
+            await addNewActivies(id, name, type, location, notes);
+            disableATPopup();
+        }
+    }
+
+    return (
+        <div id="authentication-modal" tabIndex={-1} aria-hidden="true"
+            className="overflow-y-auto overflow-x-hidden backdrop-blur-sm fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div className="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 w-full max-w-md max-h-full">
+
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+                    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                            Create New Activity
+                        </h3>
+                        <button type="button" onClick={disableATPopup} className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
+                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            </svg>
+                            <span className="sr-only" >Close modal</span>
+                        </button>
+                    </div>
+
+                    <div className="p-4 md:p-5">
+                        <form className="space-y-4" action="#">
+                            <div>
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                <input type="text" name="name" id="name" value={name} onChange={e => setName(e.target.value)}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                    placeholder="Tokyo, Japan" required />
+                            </div>
+                            <div>
+                                <label htmlFor="type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type</label>
+                                <select name="type" id="type" value={location} onChange={(e) => setType(e.target.value)}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                    required>
+                                    <option value="place">Place</option>
+                                    <option value="action">Action</option>
+                                    <option value="restaurant">Restaurant</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label htmlFor="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
+                                <input type="text" name="location" id="location" value={location} onChange={(e) => setLocation(e.target.value)}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="notes" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Notes</label>
+                                <textarea name="notes" id="notes" value={notes} onChange={e => setNotes(e.target.value)}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                />
+                            </div>
+                            {errorLog !== "" &&
+                                <div className="block mb-2 text-sm font-medium text-red-700 dark:text-red-400">{errorLog}</div>
+                            }
+                            <button type="submit" onClick={sumbitNewTrip} className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Create New Trip
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,10 +1,27 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useCallback, useState } from "react";
+import AddActivityModal from "~/app/_components/addActivityModal";
 
-export default function EventDetailPage({ params }: { params: { id: string } }) {
+export default function EventDetailPage({ params, query }: { params: { id: string }, query: { userid: string, destination: string, startDate: string, endDate: string, id: number } }) {
+    const [showAAPopup, setShowAAPopup] = useState(false);
+    const searchParams = useSearchParams();
+
+    const disableATPopup = useCallback(() => {
+        setShowAAPopup(false);
+    }, [showAAPopup]);
+
     return (
         <div className="w-9/12 mx-auto pt-8">
+            <div id="trips" className="flex pb-2 mb-4 justify-between items-center border-b-2 border-black">
+                <h2 className="text-xl font-bold">Trips</h2>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full" onClick={() => setShowAAPopup(true)}>+</button>
+            </div>
             <div className="text-lg">
                 This event id is {params.id}
+                <div className="">Morning</div>
             </div>
+            {showAAPopup && <AddActivityModal id={searchParams.get("userid")} disableATPopup={disableATPopup} />}
         </div>
     )
 }
