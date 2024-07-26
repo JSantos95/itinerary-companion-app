7ba20f48e847298ddb50ee1371c7e806e8006ce5
Jose <santosjosel1995@gmail.com>
2024-07-25 23:50:23 -0400

    Update: Event page will fetch all event activities

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import AddActivityModal from "~/app/_components/addActivityModal";
+import { DayList } from "~/app/_components/dayList";
+import { Activity } from "~/app/utilities/types";
+import { getEventActivies } from "~/server/db";
 
 export default function EventDetailPage({ params, query }: { params: { id: string }, query: { userid: string, destination: string, startDate: string, endDate: string, id: number } }) {
     const [showAAPopup, setShowAAPopup] = useState(false);
+    const [activitiesList, setActivitiesList] = useState<Array<Activity>>();
     const searchParams = useSearchParams();
 
     const disableATPopup = useCallback(() => {
         setShowAAPopup(false);
     }, [showAAPopup]);
+
+    useEffect(() => {
+        const getActs = async () => {
+            const dbFeedback = await getEventActivies(parseInt(params.id));
+            setActivitiesList(dbFeedback);
+        }
+        getActs();
+    }, []);
 
     return (
         <div className="w-9/12 mx-auto pt-8">
@@ -24,12 +36,13 @@ export default function EventDetailPage({ params, query }: { params: { id: strin
                 {/* Trip's days */}
                 <div className="w-full">
                     <div className="text-xl font-bold">Days Initerary</div>
-
+                    <DayList eventId={params.id} />
                 </div>
                 {/* Activities List */}
                 <div id="trips" className="flex w-full pb-2 mb-4 justify-between items-center border-b-2 border-black">
                     <h2 className="text-xl font-bold">Activities</h2>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full" onClick={() => setShowAAPopup(true)}>+</button>
+
                 </div>
             </div>
             {/* Add New Activity Modal */}
