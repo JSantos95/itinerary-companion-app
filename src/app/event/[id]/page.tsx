931ceed41120fb538d9b1b1e374376eb6fa0678b
Jose <santosjosel1995@gmail.com>
2024-07-27 16:11:14 -0400

    Update: styling and name change

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -4,7 +4,7 @@ import { useCallback, useEffect, useState } from "react";
 import AddActivityModal from "~/app/_components/addActivityModal";
 import { DayList } from "~/app/_components/dayList";
 import { Activity } from "~/app/utilities/types";
-import { getEventActivies } from "~/server/db";
+import { getEventActivities } from "~/server/db";
 
 export default function EventDetailPage({ params, query }: { params: { id: string }, query: { userid: string, destination: string, startDate: string, endDate: string, id: number } }) {
     const [showAAPopup, setShowAAPopup] = useState(false);
@@ -17,8 +17,11 @@ export default function EventDetailPage({ params, query }: { params: { id: strin
 
     useEffect(() => {
         const getActs = async () => {
-            const dbFeedback = await getEventActivies(parseInt(params.id));
-            setActivitiesList(dbFeedback);
+            const dbFeedback = await getEventActivities(parseInt(params.id));
+            console.log(dbFeedback);
+            if (dbFeedback != null) {
+                setActivitiesList(dbFeedback);
+            }
         }
         getActs();
     }, []);
@@ -32,14 +35,16 @@ export default function EventDetailPage({ params, query }: { params: { id: strin
             <div className="text-lg text-gray-800 pb-5">
                 {searchParams.get("startDate")} to {searchParams.get("endDate")}
             </div>
-            <div className="flex flex-row gap-y-4 grid-rows-2">
+            <div className="flex flex-row gap-y-4 gap-x-5 grid-rows-2">
                 {/* Trip's days */}
-                <div className="w-full">
-                    <div className="text-xl font-bold">Days Initerary</div>
-                    <DayList eventId={params.id} />
+                <div className="flex w-full pb-2 mb-4 justify-center items-center border-b-2 border-black">
+                    <div className="text-xl font-bold">
+                        Days Initerary
+                    </div>
+                    {/* <DayList eventId={params.id} /> */}
                 </div>
                 {/* Activities List */}
-                <div id="trips" className="flex w-full pb-2 mb-4 justify-between items-center border-b-2 border-black">
+                <div id="trips" className="flex w-full pb-2 mb-4 gap-x-3 justify-center items-center border-b-2 border-black">
                     <h2 className="text-xl font-bold">Activities</h2>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full" onClick={() => setShowAAPopup(true)}>+</button>
 
