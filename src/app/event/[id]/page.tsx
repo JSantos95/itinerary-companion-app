920101b557f3d807dd360f32fa9067b115964f98
Jose <santosjosel1995@gmail.com>
2024-07-29 23:44:27 -0400

    Fixed error, was sending userid instead of eventid Added activity list

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
+import { Activity } from "~/app/_components/activity";
 import AddActivityModal from "~/app/_components/addActivityModal";
 import { DayList } from "~/app/_components/dayList";
-import { Activity } from "~/app/utilities/types";
+import { Activity as ActivityProps } from "~/app/utilities/types";
 import { getEventActivities } from "~/server/db";
 
 export default function EventDetailPage({ params, query }: { params: { id: string }, query: { userid: string, destination: string, startDate: string, endDate: string, id: number } }) {
     const [showAAPopup, setShowAAPopup] = useState(false);
-    const [activitiesList, setActivitiesList] = useState<Array<Activity>>();
+    const [activitiesList, setActivitiesList] = useState<Array<ActivityProps>>();
     const searchParams = useSearchParams();
 
     const disableATPopup = useCallback(() => {
@@ -26,6 +27,7 @@ export default function EventDetailPage({ params, query }: { params: { id: strin
         getActs();
     }, []);
 
+    const activities = activitiesList?.map(act => <Activity {...act} />);
     return (
         <div className="w-9/12 mx-auto pt-8">
             {/* Heading */}
@@ -35,23 +37,28 @@ export default function EventDetailPage({ params, query }: { params: { id: strin
             <div className="text-lg text-gray-800 pb-5">
                 {searchParams.get("startDate")} to {searchParams.get("endDate")}
             </div>
+            {/* Titles */}
             <div className="flex flex-row gap-y-4 gap-x-5 grid-rows-2">
-                {/* Trip's days */}
                 <div className="flex w-full pb-2 mb-4 justify-center items-center border-b-2 border-black">
-                    <div className="text-xl font-bold">
-                        Days Initerary
-                    </div>
-                    {/* <DayList eventId={params.id} /> */}
+                    <div className="text-xl font-bold">Days Initerary</div>
                 </div>
-                {/* Activities List */}
                 <div id="trips" className="flex w-full pb-2 mb-4 gap-x-3 justify-center items-center border-b-2 border-black">
                     <h2 className="text-xl font-bold">Activities</h2>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full" onClick={() => setShowAAPopup(true)}>+</button>
-
+                </div>
+            </div>
+            <div className="flex w-full flex-row gap-y-4 gap-x-5 grid-rows-2">
+                {/* Trip's days */}
+                <div className="w-full">
+                    {/* <DayList eventId={params.id} /> */}
+                </div>
+                {/* Activities List */}
+                <div className="w-full">
+                    {activities}
                 </div>
             </div>
             {/* Add New Activity Modal */}
-            {showAAPopup && <AddActivityModal id={searchParams.get("userid")} disableATPopup={disableATPopup} />}
+            {showAAPopup && <AddActivityModal id={params.id} disableATPopup={disableATPopup} />}
         </div>
     )
 }
