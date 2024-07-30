08c2102b26e702842425d2fdb3d670915fe612ae
Jose <santosjosel1995@gmail.com>
2024-07-29 23:46:00 -0400

    New Activity card to display activities

---
+++ src/app/_components/activity.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import { Activity as ActivityProps } from "../utilities/types";
+import Link from "next/link";
+
+export const Activity: FC<ActivityProps> = ({ id, eventId, name, type, location, notes }) => {
+
+    return (
+        <div className="mb-3">
+            <Link href={{ pathname: "/activity/" + id, query: { id, eventId, name, type, location, notes } }}
+                className="block max-w-sm p-6 bg-white border border-blue-200 rounded-lg shadow hover:bg-blue-100 dark:bg-blue-600 dark:border-blue-600 dark:hover:bg-blue-800">
+                <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {name}
+                </p>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    {type}
+                </p>
+            </Link>
+        </div>
+    )
+}
