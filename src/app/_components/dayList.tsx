3e898eafc2a4fd89def83c957f014502877b129b
Jose <santosjosel1995@gmail.com>
2024-07-31 23:17:47 -0400

    Added Day to display TravelDay

--- src/app/_components/dayList.tsx
+++ src/app/_components/dayList.tsx
@@ -13,16 +13,37 @@ export const DayList: FC<Props> = ({ eventId }) => {
         const getActs = async () => {
             const dbFeedback = await getEventDays(parseInt(eventId));
             if (dbFeedback != null) {
-                //setDayList(dbFeedback);
+                setDayList(dbFeedback);
             }
         }
         getActs();
     }, [])
 
-
+    const dayz = dayList?.map((d, index) => <Day {...d} index={index} key={index} />);
     return (
         <div className="mb-3">
+            {dayz}
+        </div>
+    )
+}
 
+type DayProp = TravelDay & { index: number };
+
+const Day: FC<DayProp> = ({ day, index }) => {
+    return (
+        <div className="min-h-40">
+            <div className="text-2xl p-2 mb-2 w-full bg-blue-300 rounded-lg">
+                Day {index + 1}: {day.slice(5)}
+            </div>
+            <div className="min-h-20">
+                <div className="text-xl">Morning</div>
+            </div>
+            <div className="min-h-20">
+                <div className="text-xl">Afternoon</div>
+            </div>
+            <div className="min-h-20">
+                <div className="text-xl">Evening</div>
+            </div>
         </div>
     )
 }
