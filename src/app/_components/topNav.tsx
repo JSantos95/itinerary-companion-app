2064246ffc4ddeb71e1adc6e94c1fbe774830f1b
Jose <santosjosel1995@gmail.com>
2024-08-01 22:36:46 -0400

    Title links to the homepage

--- src/app/_components/topNav.tsx
+++ src/app/_components/topNav.tsx
@@ -4,11 +4,14 @@ import {
     SignedOut,
     UserButton
 } from '@clerk/nextjs'
+import Link from 'next/link';
 
 export function TopNav() {
     return (
         <nav className="flex w-flex p-3 items-center justify-between border-b-4 border-b-black text-xl font-semibold">
-            <div>Itinerary Campanion App</div>
+            <Link href={'/'}>
+                <div>Itinerary Campanion App</div>
+            </Link>
             <div className='flex justify-center content-center flex-col'>
                 <SignedOut>
                     <SignInButton />
