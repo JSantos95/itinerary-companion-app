89333f15522fb266b7a4664813b4f130ebabc203
Jose <101306680+JSantosCEI@users.noreply.github.com>
2024-12-30 20:55:28 -0500

    Starting adding the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,41 @@
 import {
   SignedIn,
-  SignedOut
+  SignedOut,
+  SignInButton,
 } from '@clerk/nextjs'
 import UserHomePage from './_components/userHomePage';
 
 export default function HomePage() {
   return (
-    <main className="p-3">
+    <main>
       <SignedOut>
-        <div className="h-full w-full text-center text-2xl">
-          Sign up above
+        <div className='bg-gradient-to-tl from-gray-400 to-gray-100 h-96 w-full relative'>
+          <img src="https://utfs.io/f/rDt6ewO3q1fSOrtNyDu7skFGo5Bc2VOxmaXhTq6zJZUwDnEe" alt="bg-image"
+            className='w-full h-full object-cover absolute mix-blend-overlay' />
+          {/* Hero End */}
+          <div className='h-full w-full flex justify-center text-center text-2xl'>
+            <div className='w-1/2 pt-20'>
+              <div className='text-5xl font-bold'>
+                Plan out your next trip, event, or vacation here.
+              </div>
+              <div className='text-lg pt-5'>
+                Laid back intinerary planner that helps you save and oraginize your day by day activites
+              </div>
+              <div className='pt-5'>
+                <SignInButton >
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">
+                    Get started
+                  </button>
+                </SignInButton>
+              </div>
+            </div>
+          </div>
+          {/* Hero End */}
         </div>
+
       </SignedOut>
       <SignedIn>
-        <div>
+        <div className='p-3'>
           <UserHomePage />
         </div>
       </SignedIn>
