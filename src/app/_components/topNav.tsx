fd6b53672ab96cb4cf55cac021f98b7b67f532c1
Jose <101306680+JSantosCEI@users.noreply.github.com>
2025-01-02 09:58:19 -0500

    Made navbar absolute

--- src/app/_components/topNav.tsx
+++ src/app/_components/topNav.tsx
@@ -8,18 +8,24 @@ import Link from 'next/link';
 
 export function TopNav() {
     return (
-        <nav className="flex w-flex p-3 items-center justify-between border-b-4 border-b-black text-xl font-semibold">
-            <Link href={'/'}>
-                <div>Itinerary Campanion</div>
-            </Link>
-            <div className='flex justify-center content-center flex-col'>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
+        <div className='relative h-full w-full'>
+            <div className='absolute w-full top-0 z-50'>
+                <nav className="flex w-flex p-3 items-center justify-between border-b-2 border-b-black text-xl font-semibold bg-slate-100">
+                    <Link href={'/'}>
+                        <div>Itinerary Campanion</div>
+                    </Link>
+                    <div className='flex justify-center content-center flex-col'>
+                        <SignedOut>
+                            <SignInButton />
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </div>
+                </nav>
             </div>
-        </nav>
+
+        </div>
+
     );
 }
